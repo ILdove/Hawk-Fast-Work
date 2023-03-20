33acc2d6efd117c35e7a654181f2dd579fb5611c
yangchen <magivdva9812@gmail.com>
2023-03-20 11:57:56 +0800

    新建常量池

---
+++ hawk-common/src/main/java/com/hawk/common/pool/ConstantPool.java
@@ -0,0 +1,13 @@
+package com.hawk.common.pool;
+
+/**
+ * 常量池
+ *
+ * @author 杨晨
+ * @date 2023-03-20
+ */
+public interface ConstantPool {
+
+    String STRING_ZERO = "0";
+    Integer INTEGER_ZERO = 0;
+}
